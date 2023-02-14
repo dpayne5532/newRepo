d90efb03bf9d4aca22f9bf762b5542eec64bbdad
Dan Payne <dpaynebills@gmail.com>
2023-02-13 20:03:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // changing shit
+const express = require('express')
+const app = express()
+
+
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`----====|||   Server Listening on PORT ${PORT}   |||====----`) )
